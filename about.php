7acea806d8cd5dbd6f4a378559facab3f84caea9
Abdulrahman Elwakeel <abdulrahmanmohamedelwakeel@gmail.com>
2026-05-11 18:19:43 +0300

    Refactor about.php to improve formatting

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 <section class="about">
   <h1>About Our Clinic</h1>
   <p>Our clinic is dedicated to providing the highest quality healthcare services. With a team of experienced doctors and state-of-the-art facilities, we ensure that every patient receives the best care possible.</p>
-  <p>Our mission is to improve the health and well-being of our community through professional medical services and compassionate care.</p>
+  <p>Our mission is to improve the health and well being of our community through professional medical services and compassionate care.</p>
   <img src="assets/images/AAAAAA.jpg" alt="Clinic">
 </section>
 </main>
